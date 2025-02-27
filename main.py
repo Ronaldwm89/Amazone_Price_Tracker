f314dda674ddb15bcfa708dd2937d6a87d0bd830
Ronald <ronaldwmm1989@gmail.com>
2025-02-27 18:15:58 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup 
+
+url = "https://appbrewery.github.io/instant_pot/"
+response = requests.get(url)
+website_soup = BeautifulSoup(response.text, "html.parser")
+
+precio = website_soup.find('span', class_='a-price-whole').getText()
+centimos = website_soup.find('span', class_='a-price-fraction').getText()
+
+precio_total = float(precio) + float(centimos) / 100
+print(precio_total + 318.25)
+
+
