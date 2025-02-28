edb1156ae3ac69670a70e6ce1efe14a799c8badc
Ronald <ronaldwmm1989@gmail.com>
2025-02-28 18:20:24 -0300

    Updates

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import requests
-from bs4 import BeautifulSoup 
+import smtplib 
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
 
-url = "https://appbrewery.github.io/instant_pot/"
-response = requests.get(url)
-website_soup = BeautifulSoup(response.text, "html.parser")
+load_dotenv()
 
-precio = website_soup.find('span', class_='a-price-whole').getText()
-centimos = website_soup.find('span', class_='a-price-fraction').getText()
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
-precio_total = float(precio) + float(centimos) / 100
+print(f"Email: {EMAIL}")
 
+def get_price():
+    url = "https://appbrewery.github.io/instant_pot/"
+    response = requests.get(url)
+    website_soup = BeautifulSoup(response.text, "html.parser")
+
+    precio = website_soup.find('span', class_='a-price-whole').getText()
+    centimos = website_soup.find('span', class_='a-price-fraction').getText()
+
+    precio_total = float(precio) + float(centimos) / 100
+    
+    return precio_total
+
+def send_email():
+    COST = get_price()
+    #MY_EMAIL = 
+    #MY_PASSWORD = 
+
+    #if COST < 100:
+        #with smtplib**
 
