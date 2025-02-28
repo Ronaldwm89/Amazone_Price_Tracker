5f66611039d51da483935269642965eedde5d27d
Ronald <ronaldwmm1989@gmail.com>
2025-02-28 19:51:58 -0300

    Including Headers

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ import os
 
 load_dotenv() # Load environment variables
 
+# Headers to avoid being blocked
+headers = {
+
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7"
+}
+
+
 # Constants data from .env file
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
+URL = "https://appbrewery.github.io/instant_pot/"
 
 # Function to get the price of the product
 def get_price():
-    url = "https://appbrewery.github.io/instant_pot/"
-    response = requests.get(url)
+    response = requests.get(URL, headers=headers)
+    response.raise_for_status()
     website_soup = BeautifulSoup(response.text, "html.parser")
 
     precio = website_soup.find('span', class_='a-price-whole').getText()
@@ -30,9 +40,8 @@ def create_email():
     msg['Subject'] = '!!ALERT!! It´s time to buy'
     msg['From'] = EMAIL
     msg['To'] = 'ronmed1989@yahoo.com'
-    msg.set_content('Time to buy, the product is under 100 you should buy it now')
+    msg.set_content(f'Time to buy, the product is under 100$ you should buy it now\n {URL}')
     return msg
-
 
 #Function to send the email
 def send_email():
@@ -46,5 +55,4 @@ def send_email():
             server.send_message(MSG)
 
 send_email()
-
 
